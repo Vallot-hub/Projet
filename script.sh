e7461c408df4a0cfba6a62b551874057ee7466c7
Vallot-hub <59056759+Vallot-hub@users.noreply.github.com>
2020-03-02 14:17:52 +0100

    Add files via upload

--- script.sh
+++ script.sh
@@ -10,11 +10,12 @@ do
 #echo $donnee                         //test
 Compteur=$(echo $donnee | cut -d: -f1)
 Electrovanne=$(echo $donnee | cut -d: -f2)
+Debit=$(echo $donnee | cut -d: -f3)
+
 #echo $Compteur et $Electrovanne          //test
 
-#Ce connecte à la base de donnée a l'aide de commande mysql(mysql -u root --password=root -e) et sql(USE Projet; INSERT INTO Eau (Conso,Total,Electrovanne) VALUES ($donnee,2,1);)
+#Ce connecte à la base de donnée a l'aide de la commande mysql(mysql -u root --password=root -e) et sql(USE Projet; INSERT INTO Eau (Conso,Total,Electrovanne) VALUES ($donnee,2,1);)
 
-mysql -u root --password=root -e "USE Projet; INSERT INTO Eau (Conso,Total,Electrovanne) VALUES ($Compteur,2,$Electrovanne);"
+mysql -u api --password=snir -e "USE Projet; INSERT INTO Eau (Conso,Total,Electrovanne) VALUES ($Compteur,$Debit,$Electrovanne);"
 
 done
-

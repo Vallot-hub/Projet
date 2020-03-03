762e1679ae3a0c65ad06c55590e55b6d85dde631
Vallot-hub <59056759+Vallot-hub@users.noreply.github.com>
2020-03-03 22:24:16 +0100

    Add files via upload

---
+++ eau/raspberry/Mqttscript.sh
@@ -0,0 +1,21 @@
+#! /bin/bash
+
+
+#..............Nom d'hote..........Nom du topic......copie les donnes dans la variable..#
+mosquitto_sub -h 127.0.0.1 -t compteur_connecte/conso | while read donnee
+
+do
+
+
+#echo $donnee                         //test
+Compteur=$(echo $donnee | cut -d: -f1)
+Electrovanne=$(echo $donnee | cut -d: -f2)
+Debit=$(echo $donnee | cut -d: -f3)
+
+#echo $Compteur et $Electrovanne          //test
+
+#Ce connecte à la base de donnée a l'aide de la commande mysql(mysql -u root --password=root -e) et sql(USE Projet; INSERT INTO Eau (Conso,Total,Electrovanne) VALUES ($donnee,2,1);)
+
+mysql -u api --password=snir -e "USE Projet; INSERT INTO Eau (Conso,Total,Electrovanne) VALUES ($Compteur,$Debit,$Electrovanne);"
+
+done
